0da227a8f6480bb2387c127b70771ee6a5e33be0
Ali <76146196+Boyface07@users.noreply.github.com>
2021-01-17 11:33:27 +0700

    Add files via upload

--- anykernel.sh
+++ anykernel.sh
@@ -4,15 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=YOU'VE JOINED THE ROCKET
+kernel.string=YOU JOINED TO ROCKET!!
 do.devicecheck=1
 do.modules=0
-do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=lavender
 device.name2=lavender
-supported.versions=
+supported.versions=9 - 11
 supported.patchlevels=
 '; } # end properties
 
@@ -26,8 +25,17 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
 ## AnyKernel install
 dump_boot;
+
+# begin ramdisk changes
 
 # end ramdisk changes
 
